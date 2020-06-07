2ff35fef1287ed13688ba06ba1ee84b254ce7324
Slava <urigami2010@gmail.com>
2020-06-07 17:34:41 +0300

    trying to launch

--- slack_bot.py
+++ slack_bot.py
@@ -1,38 +1,18 @@
 import flask
-from datetime import datetime
-import requests
-import json
-from apscheduler.schedulers.background import BackgroundScheduler
-
-# settings.
-super_secret_web_hook = 'https://hooks.slack.com/services/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'
-
-
-################################################################################
-# Schedule of sending a message.##############################
-def schedule_send_time_request():
-    requests.post(super_secret_web_hook, json.dumps({'text': f'{datetime.now()}'}))
-
-
-scheduler = BackgroundScheduler()
-scheduler.add_job(func=schedule_send_time_request, trigger="interval", seconds=600)
-scheduler.start()
 
 app = flask.Flask(__name__)
-app.config["DEBUG"] = True
-################################################################################
 
 # Methods
-@app.route('/', methods=['GET'])
+@app.route('/')
 def home():
-    requests.post(super_secret_web_hook, json.dumps({'text': f'{datetime.now()}'}))
+
     return 'Time sent!'
 
 
-@app.route('/time', methods=['ANY'])
+@app.route('/time')
 def time():
-    return f"{datetime.now()}"
+    return 'Yo!'
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
