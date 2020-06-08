a0a9bfe1151c259fb1dacc0d31c4f588da62b119
Slava <urigami2010@gmail.com>
2020-06-08 15:31:04 +0300

    Done with twitter

--- app.py
+++ app.py
@@ -47,29 +47,29 @@ scheduler = BackgroundScheduler()
 scheduler.add_job(func=schedule_send_time_request, trigger="interval", seconds=seconds_interval_per_update)
 scheduler.start()
 
-# if run_stream_with_user:
-#     class TwitterListener(StreamListener):
-#         def on_data(self, data):
-#             try:
-#                 data = json.loads(data)
-#                 requests.post(super_secret_web_hook, json.dumps({"text": f'{data["text"]}'}))
-#                 logging.info(f'User has posted an update : {data["text"]}')
-#                 return True
-#             except:
-#                 logging.error(f'There was no data while following the user.')
-#
-#         def on_error(self, status_code):
-#             logging.warning(f'Something went wrong while following Twitter.{status_code}')
-#
-#
-#     """
-#     This won't work on Heroku free account, this WILL work locally, or on a proper server.
-#     change to True to make it work locally, this starts the Streaming (Following) for the user account, and gives you
-#     instant updates on new tweets from the selected user.
-#     """
-#     listener = TwitterListener()
-#     stream = Stream(auth, listener)
-#     stream.filter(follow=[user_id])
+if run_stream_with_user:
+    class TwitterListener(StreamListener):
+        def on_data(self, data):
+            try:
+                data = json.loads(data)
+                requests.post(super_secret_web_hook, json.dumps({"text": f'{data["text"]}'}))
+                logging.info(f'User has posted an update : {data["text"]}')
+                return True
+            except:
+                logging.error(f'There was no data while following the user.')
+
+        def on_error(self, status_code):
+            logging.warning(f'Something went wrong while following Twitter.{status_code}')
+
+
+    """
+    This won't work on Heroku free account, this WILL work locally, or on a proper server.
+    change to True to make it work locally, this starts the Streaming (Following) for the user account, and gives you
+    instant updates on new tweets from the selected user.
+    """
+    listener = TwitterListener()
+    stream = Stream(auth, listener)
+    stream.filter(follow=[user_id])
 
 
 def send_update(username, user_full_name):
@@ -80,77 +80,110 @@ def send_update(username, user_full_name):
 
     :return:
     """
-    response = ''
-    for tweet in tweepy.Cursor(api.user_timeline, screen_name=username).items(5):
-        if tweet.created_at > datetime.datetime.now() - datetime.timedelta(hours=latest_tweets_time):
-            response = response + f'*•* {tweet.text} \n\n'
-    if response != '':
-        requests.post(super_secret_web_hook,
-                      json.dumps({"text": f'>:robot_face::speech_balloon: *News from {user_full_name}!*'}))
-        requests.post(super_secret_web_hook, json.dumps({"text": f'{response}'}))
-    logging.info('Sent an update!')
-    return f'{username} News!:'
+    try:
+        response = ''
+        for tweet in tweepy.Cursor(api.user_timeline, screen_name=username).items(5):
+            if tweet.created_at > datetime.datetime.now() - datetime.timedelta(hours=latest_tweets_time):
+                response = response + f'*•* {tweet.text} \n\n'
+        if response != '':
+            requests.post(super_secret_web_hook,
+                          json.dumps({"text": f'>:robot_face::speech_balloon: *News from {user_full_name}!*'}))
+            requests.post(super_secret_web_hook, json.dumps({"text": f'{response}'}))
+        logging.info('Sent an update!')
+        return f'{username} News!:'
+    except:
+        logging.error("Something is wrong with sending a message")
+        return False
 
 
 @app.route('/python-weekly', methods=['POST'])
 def retrieve_python_weekly():
-    send_update('PythonWeekly', 'Python Weekly')
-    return flask.Response()
-
+    try:
+        send_update('PythonWeekly', 'Python Weekly')
+        return True
+    except:
+        logging.error("Something is wrong with '/python-weekly'")
+        return False
 
 @app.route('/real-python', methods=['POST'])
 def retrieve_real_python():
-    send_update('RealPython', 'Real Python')
-    return flask.Response()
-
+    try:
+        send_update('RealPython', 'Real Python')
+        return True
+    except:
+        logging.error("Something is wrong with '/real-python'")
+        return False
 
 @app.route('/python-hub', methods=['POST'])
 def retrieve_python_hub():
-    send_update('PythonHub', 'Python Hub')
-    return flask.Response()
-
+    try:
+        send_update('PythonHub', 'Python Hub')
+        return True
+    except:
+        logging.error("Something is wrong with '/python-hub'")
+        return False
 
 @app.route('/fullstack-python', methods=['POST'])
 def retrieve_fullstackpython():
-    send_update('fullstackpython', 'Full Stack Python')
-    return flask.Response()
-
+    try:
+        send_update('fullstackpython', 'Full Stack Python')
+        return True
+    except:
+        logging.error("Something is wrong with '/fullstack-python'")
+        return False
 
 @app.route('/csharpstack', methods=['POST'])
 def retrieve_csharpstack():
-    send_update('csharpstack','C# StackOverflow')
-    return flask.Response()
-
+    try:
+        send_update('csharpstack','C# StackOverflow')
+        return True
+    except:
+        logging.error("Something is wrong with '/csharpstack'")
+        return False
 
 @app.route('/javascriptdaily', methods=['POST'])
 def retrieve_javascriptdaily():
-    send_update('JavaScriptDaily', 'JavaScript Daily')
-    return flask.Response()
-
+    try:
+        send_update('JavaScriptDaily', 'JavaScript Daily')
+        return True
+    except:
+        logging.error("Something is wrong with '/javascriptdaily'")
+        return False
 @app.route('/cprogramming1', methods=['POST'])
 def retrieve_cprogramming1():
-    send_update('CProgramming1','C++ Programming')
-    return flask.Response()
-
+    try:
+        send_update('CProgramming1','C++ Programming')
+        return True
+    except:
+        logging.error("Something is wrong with '/cprogramming1'")
+        return False
 
 @app.route('/updates', methods=['POST'])
 def get_updates():
-    schedule_send_time_request()
-    return flask.Response()
+    try:
+        schedule_send_time_request()
+        return True
+    except:
+        logging.error("Something is wrong with '/updates'")
+        return False
 
 @app.route('/tweet', methods=['POST'])
 def post_tweet():
-    print(flask.request)
-    # api.update_status("TEXT")
-    return flask.Response()
-
+    try:
+        api.update_status(flask.request.data)
+        return True
+    except:
+        logging.error("Something is wrong with '/tweet'")
+        return False
 
 @app.route('/time', methods=['POST'])
 def time():
-    requests.post(super_secret_web_hook, json.dumps({'text': f' Current time: *{datetime.datetime.now()}*'}))
-    logging.info('Sent an time!')
-    return flask.Response()
-
+    try:
+        requests.post(super_secret_web_hook, json.dumps({'text': f' Current time: *{datetime.datetime.now()}*'}))
+        return True
+    except:
+        logging.error("Something is wrong with '/time'")
+        return False
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False)
--- test.py
+++ test.py
@@ -1,33 +1,15 @@
-from tweepy import OAuthHandler
-from tweepy import Stream
-from tweepy.streaming import StreamListener
-import tweepy
-import os
+import unittest
 import requests
-twitter_consumer_key = os.environ.get('TCK')
-twitter_consumer_secret = os.environ.get('TCS')
-twitter_access_token = os.environ.get('TAT')
-twitter_access_secret = os.environ.get('TAS')
 
-auth = tweepy.OAuthHandler(twitter_consumer_key, twitter_consumer_secret)
-auth.set_access_token(twitter_access_token, twitter_access_secret)
 
-auth.secure = True
+class TestNewTweet(unittest.TestCase):
 
-api = tweepy.API(auth)
+    def test_new(self):
+        post = 'Python Tweet!'
+        requests.post('http://127.0.0.1:5000/tweet', post)
 
-class TwitterListener(StreamListener):
-    def on_data(self,data):
-        print(data)
-        return True
-    def on_error(self, status_code):
-        print(status_code)
 
-post = input('Your tweet!: ')
-requests.post('http://127.0.0.1:5000/tweet', post)
+
 
 if __name__ == '__main__':
-    listener = TwitterListener()
-
-    stream = Stream(auth, listener)
-    stream.filter(follow=['1269651960954748930']).items(5)
+    unittest.main()
