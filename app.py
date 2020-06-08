64abd2ee0cdfc2871050d3b7a044931c4788f6d4
Slava <urigami2010@gmail.com>
2020-06-08 11:10:22 +0300

    new test

--- app.py
+++ app.py
@@ -45,22 +45,22 @@ scheduler = BackgroundScheduler()
 scheduler.add_job(func=schedule_send_time_request, trigger="interval", seconds=seconds_interval_per_update)
 scheduler.start()
 
-# class TwitterListener(StreamListener):
-#     def on_data(self, data):
-#         try:
-#             data = json.loads(data)
-#             requests.post(super_secret_web_hook, json.dumps({"text": f'{data["text"]}'}))
-#             logging.info(f'User has posted an update : {data["text"]}')
-#             return True
-#         except:
-#             logging.error(f'There was no data while following the user.')
-#     def on_error(self, status_code):
-#         logging.warning(f'Something went wrong while following Twitter.{status_code}')
-#
-#
-# listener = TwitterListener()
-# stream = Stream(auth, listener)
-# stream.filter(follow=[user_id])
+class TwitterListener(StreamListener):
+    def on_data(self, data):
+        try:
+            data = json.loads(data)
+            requests.post(super_secret_web_hook, json.dumps({"text": f'{data["text"]}'}))
+            logging.info(f'User has posted an update : {data["text"]}')
+            return True
+        except:
+            logging.error(f'There was no data while following the user.')
+    def on_error(self, status_code):
+        logging.warning(f'Something went wrong while following Twitter.{status_code}')
+
+
+listener = TwitterListener()
+stream = Stream(auth, listener)
+stream.filter(follow=[user_id])
 
 def send_update(username):
     """
@@ -113,7 +113,7 @@ def get_updates():
 
 @app.route('/time', methods=['POST'])
 def time():
-    requests.post(super_secret_web_hook, json.dumps({'text': f'*{datetime.datetime.now()}*'}))
+    requests.post(super_secret_web_hook, json.dumps({'text': f' Current time: *{datetime.datetime.now()}*'}))
     logging.info('Sent an time!')
     return flask.Response()
 
