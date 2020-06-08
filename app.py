1f9cce78409f32e63a8d5124e8d31c5391471551
Slava <urigami2010@gmail.com>
2020-06-08 10:48:12 +0300

    new test

--- app.py
+++ app.py
@@ -1,41 +1,49 @@
 import datetime
 import tweepy
+from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
 import flask
 import json
 import os
 import requests
+import logging
 from apscheduler.schedulers.background import BackgroundScheduler
 
-# settings.
+# Global settings.
+logging.basicConfig(filename='LOGGER.log', level=logging.DEBUG)  # Logger
 super_secret_web_hook = os.environ.get('WEBHOOK')  # Slack Webhook
+seconds_interval_per_update = 3600  # Time interval per update
+latest_tweets_time = 4  # The time from where we want tweets(1 hour ago, means 4 hours ago because of time diffs in IL).
+app = flask.Flask(__name__)  # Flask App
 
-twitter_consumer_key = os.environ.get('TCK')
-twitter_consumer_secret = os.environ.get('TCS')
-twitter_access_token = os.environ.get('TAT')
-twitter_access_secret = os.environ.get('TAS')
+# Twitter Settings
+twitter_consumer_key = os.environ.get('TCK')  # Consumer Key
+twitter_consumer_secret = os.environ.get('TCS')  # Consumer Secret
+twitter_access_token = os.environ.get('TAT')  # Access Token
+twitter_access_secret = os.environ.get('TAS')  # Access Secret
+auth = tweepy.OAuthHandler(twitter_consumer_key, twitter_consumer_secret)   # Twitter auth handler
+auth.set_access_token(twitter_access_token, twitter_access_secret)  # Setting access Token
+user_id = '1269651960954748930'  # The ID of the user that the bot will follow, and get his latest updates.
+api = tweepy.API(auth)  # connection to Twitter API.
 
-auth = tweepy.OAuthHandler(twitter_consumer_key, twitter_consumer_secret)
-auth.set_access_token(twitter_access_token, twitter_access_secret)
-
-api = tweepy.API(auth)
-
-app = flask.Flask(__name__)
 
 def schedule_send_time_request():
     """
-    Sends a message to the slack bot every period of time.
+    Makes the bot to send an update every period of time (Set by user above).
     :return:
     """
-    requests.post(super_secret_web_hook, json.dumps({'text': f'*Current Time: {datetime.datetime.now()}*'}))
+    requests.post(
+        super_secret_web_hook,
+        json.dumps({'text': f'> :robot_face::speech_balloon:*  Latest Updates:* *Time: {datetime.datetime.now()}*'}))
     send_update('PythonWeekly')
     send_update('RealPython')
     send_update('PythonHub')
     send_update('PythonWeekly')
 
 
-
 scheduler = BackgroundScheduler()
-scheduler.add_job(func=schedule_send_time_request, trigger="interval", seconds=3600)
+scheduler.add_job(func=schedule_send_time_request, trigger="interval", seconds=seconds_interval_per_update)
 scheduler.start()
 
 
@@ -46,12 +54,15 @@ def send_update(username):
 
     :return:
     """
-
+    response = ''
     for tweet in tweepy.Cursor(api.user_timeline, screen_name=username).items(5):
-        if tweet.created_at > datetime.datetime.now() - datetime.timedelta(hours=4):
-            requests.post(super_secret_web_hook,
-                json.dumps({"text": f"> :robot_face::speech_balloon:*  A new tweet from {username} !! BEEP*"}))
-            requests.post(super_secret_web_hook, json.dumps({"text": f'{tweet.text}'}))
+        if tweet.created_at > datetime.datetime.now() - datetime.timedelta(hours=latest_tweets_time):
+            response = response + f'*•* {tweet.text} \n\n'
+    if response != '':
+        requests.post(super_secret_web_hook,
+                      json.dumps({"text": f'>:robot_face::speech_balloon: *News from {username}!*'}))
+        requests.post(super_secret_web_hook, json.dumps({"text": f'{response}'}))
+    logging.info('Sent an update!')
     return f'{username} News!:'
 
 
@@ -66,26 +77,48 @@ def retrieve_real_python():
     send_update('RealPython')
     return flask.Response()
 
+
 @app.route('/python-hub', methods=['POST'])
 def retrieve_python_hub():
     send_update('PythonHub')
     return flask.Response()
+
 
 @app.route('/fullstack-python', methods=['POST'])
 def retrieve_fullstackpython():
     send_update('PythonWeekly')
     return flask.Response()
 
+
 @app.route('/updates', methods=['POST'])
 def get_updates():
     schedule_send_time_request()
     return flask.Response()
 
+
 @app.route('/time', methods=['POST'])
 def time():
     requests.post(super_secret_web_hook, json.dumps({'text': f'*{datetime.datetime.now()}*'}))
+    logging.info('Sent an time!')
     return flask.Response()
 
+
+class TwitterListener(StreamListener):
+    def on_data(self, data):
+        try:
+            data = json.loads(data)
+            requests.post(super_secret_web_hook, json.dumps({"text": f'{data["text"]}'}))
+            logging.info(f'User has posted an update : {data["text"]}')
+            return True
+        except:
+            logging.error(f'There was no data while following the user.')
+    def on_error(self, status_code):
+        logging.warning(f'Something went wrong while following Twitter.{status_code}')
+
+
+listener = TwitterListener()
+stream = Stream(auth, listener)
+stream.filter(follow=[user_id])
 
 if __name__ == '__main__':
     app.run(debug=False, use_reloader=False)
--- test.py
+++ test.py
@@ -1,4 +1,30 @@
-import requests
+from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+import tweepy
+import os
 
-ans = requests.post('http://127.0.0.1:5000/python-hub')
-print(ans.content)
+twitter_consumer_key = os.environ.get('TCK')
+twitter_consumer_secret = os.environ.get('TCS')
+twitter_access_token = os.environ.get('TAT')
+twitter_access_secret = os.environ.get('TAS')
+
+auth = tweepy.OAuthHandler(twitter_consumer_key, twitter_consumer_secret)
+auth.set_access_token(twitter_access_token, twitter_access_secret)
+
+auth.secure = True
+
+api = tweepy.API(auth)
+
+class TwitterListener(StreamListener):
+    def on_data(self,data):
+        print(data)
+        return True
+    def on_error(self, status_code):
+        print(status_code)
+
+if __name__ == '__main__':
+    listener = TwitterListener()
+
+    stream = Stream(auth, listener)
+    stream.filter(follow=['1269651960954748930']).items(5)
